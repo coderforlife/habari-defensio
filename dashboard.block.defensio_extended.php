f6bffa5720f4ba3e50aadf352325fa6640f6b10f
coderforlife <jeff@coderforlife.com>
2013-03-01 15:49:44 -0800

    Cleaning up.

--- dashboard.block.defensio_extended.php
+++
@@ -1,8 +0,0 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
-<?php
-if ( $content->error_msg ) {
-    echo $content->error_msg;
-} else {
-    echo "<img src='{$content->charts['recent-accuracy']}' style='max-width:100%;max-height:100%;'>";
-}
-?>
